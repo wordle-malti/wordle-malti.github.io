6fb5adf06d1e0f3d48c73383e4d18f377b9a2faf
Franco Cassar Manghi <me@fcm.mt>
2022-01-25 22:48:25 +0000

    Fixed issues with dictionary refresh around length calculation with special Maltese characters as well as missing UTF8 declaration causing inconsistent behaviour.

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export LANG=C.UTF-8
 export LESSCHARSET=utf-8
 
 URL="https://mlrs.research.um.edu.mt/resources/gabra-api/data/gabra_data_2022-01-01.tar.gz"
@@ -23,11 +24,19 @@ cat wordforms.json | jq -r '.surface_form' | sort | uniq | awk '{print tolower($
 
 sed -i '/ /d' surface_forms.txt # clean up multi-worded entries (lines containing spaces)
 
+sed -i 's/ie/\?/g' surface_forms.txt # convert 'ie' into a temporary character as it needs to be counted as 1 char not 2
+
+sed -i 's/għ/\//g' surface_forms.txt # convert 'għ' into a temporary character as it needs to be counted as 1 char not 2
+
 sed -i '/.\{6\}/d' surface_forms.txt # clean up words longer than 5 characters
 
 sed -i '/^.\{0,4\}$/d' surface_forms.txt # clean up words shorter than 5 characters
 
-jq -R -s -c 'split("\n")' < surface_forms.txt | jq > dictionary.json
+sed -i 's/\?/ie/g' surface_forms.txt # convert '?' back to 'ie' now that length filtering is done
+
+sed -i 's/\//għ/g' surface_forms.txt # convert '/' back to 'għ' now that length filtering is done
+
+jq -R -s -c 'split("\n")' < surface_forms.txt | jq 'map(select(length > 0))' > dictionary.json
 cp dictionary.json /tmp/werdil/site/dictionary.json
 
 echo "Dictionary populated and stored under site/dictionary.json."
